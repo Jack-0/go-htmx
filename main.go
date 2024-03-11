a802bd89d6a6f069aebcd48b4f8da8b8a40f6d13
Jack <github.ay8yf@slmail.me>
2024-03-11 21:21:20 +0000

    implemented basic timetable view

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/a-h/templ"
-	"local/htmx-tt/components"
+	"local/htmx-tt/templates/components"
+	"net/http"
 )
 
 type GlobalState struct {
@@ -12,40 +12,10 @@ type GlobalState struct {
 }
 
 var global GlobalState
-	
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	component := components.Counter(global.Count, 0)
-	component.Render(r.Context(), w)
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	// Update state.
-	r.ParseForm()
-
-	// Check to see if the global button was pressed.
-	if r.Form.Has("global") {
-		global.Count++
-	}
-	//TODO: Update session.
-
-	// Display the form.
-	getHandler(w, r)
-}
-
 
 func main() {
-	component := components.Hello("Jack")
-	
-	http.Handle("/", templ.Handler(component))
-	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			postHandler(w, r)
-			return
-		}
-		getHandler(w, r)
-	})
 
+	http.Handle("/", templ.Handler(components.TimeTable(components.CreateTimetable(12))))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
