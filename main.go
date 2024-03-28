c173dfd3d72cc3d2b17c1d1c0350aa75389bc3b0
Jack <github.ay8yf@slmail.me>
2024-03-28 22:24:42 +0000

    add assets

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ var global GlobalState
 
 func main() {
 
-	http.Handle("/", templ.Handler(components.TimeTable(components.CreateTimetable(12))))
+	http.Handle("/", templ.Handler(components.TimeTable(components.CreateTimestable(12))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
